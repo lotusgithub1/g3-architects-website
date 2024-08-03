b9709a67befbaf778f8a93f37aee541906e4739f
lotus <lotuspersonal@outlook.com>
2024-08-03 12:11:47 +0600

    google work-sans font setup

---
+++ with.js
@@ -0,0 +1,18 @@
+
+
+    function showMe(){
+    with(document.customer){
+
+    var alpha=fname.value;
+    var beta=lname.value;
+    var gamma=address.value;
+    var delta=city.value;
+    var epsilon=state.value;
+  }
+  var fullName=alpha+" "+beta+"\n";
+  var livesHere=gamma+" \n"+epsilon+"\n"+delta;
+  alert(fullName+livesHere);
+
+  }
+  
+  
